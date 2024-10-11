e51a22a9dbf234af6051106569ceeb709c3606e7
Pardis Habibi <ec22566@qmul.ac.uk>
2024-10-11 22:32:43 +0100

    Making Enemies spawn
    Enemies spawn and stay in place. Must manually set the number of enemies spawning.

---
+++ Assets/scripts/Positions.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Positions : MonoBehaviour
+{
+    public static Transform[] positions;
+
+    private void Awake()
+    {
+        positions = new Transform[transform.childCount];
+        for ( int i = 0; i < positions.Length; i++)
+        {
+            positions[i] = transform.GetChild(i);
+        }
+    }
+}
