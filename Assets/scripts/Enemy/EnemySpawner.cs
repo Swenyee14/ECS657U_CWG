e6dc6b2c1554146d47cb30cad315bac4ff2b6230
Swenyee Htut Maung <120029812+Swenyee14@users.noreply.github.com>
2024-11-03 23:31:36 +0000

    added a wave system
    Can be linked in future updates. Current values are "stock" but will be able to change with global variables that must be held in a game master.

--- Assets/scripts/Enemy/EnemySpawner.cs
+++ Assets/scripts/Enemy/EnemySpawner.cs
@@ -6,25 +6,9 @@ public class EnemySpawner : MonoBehaviour
 {
     public GameObject EnemyPrefab; 
 
-    void Start()
-    {
-        Time.timeScale = 1f;
-        // call enemy spawning function
-        StartCoroutine(SpawnEnemies()); 
-    }
 
-    private IEnumerator SpawnEnemies()
-    {
-        int EnemyCount = 15;
-
-        // instantiate until the enemies is less than the enemy count
-        for (int i = 0; i < EnemyCount; i++)
-        {
-            // spawn enemies every 3 seconds
-            yield return new WaitForSeconds(3f);
-            Instantiate(EnemyPrefab, new Vector3(-9.7f, 0.46f, 7.991f), Quaternion.identity);
-        }
-
-       
+    public void SpawnEnemies()
+    { 
+        Instantiate(EnemyPrefab, new Vector3(-9.7f, 0.46f, 7.991f), Quaternion.identity);
     }
 }
