bc55ba39124a42ba017bcf144df49609e958b596
Swenyee Htut Maung <120029812+Swenyee14@users.noreply.github.com>
2024-12-08 23:16:43 +0000

    comments

--- Assets/scripts/Enemy/EnemySpawner.cs
+++ Assets/scripts/Enemy/EnemySpawner.cs
@@ -9,17 +9,19 @@ public class EnemySpawner : MonoBehaviour
     public GameObject EnemyPrefabTank;
 
 
-
+    //spawn normal enemies
     public void SpawnEnemies()
     { 
         Instantiate(EnemyPrefab, new Vector3(-9.7f, 0.46f, 7.991f), Quaternion.identity);
     }
 
+    //spawn fast enemies
     public void SpawnEnemiesFast()
     {
         Instantiate(EnemyPrefabFast, new Vector3(-9.7f, 0.46f, 7.991f), Quaternion.identity);
     }
 
+    //spawn tank enemies
     public void SpawnEnemiesTank()
     {
         Instantiate(EnemyPrefabTank, new Vector3(-9.7f, 0.46f, 7.991f), Quaternion.identity);
