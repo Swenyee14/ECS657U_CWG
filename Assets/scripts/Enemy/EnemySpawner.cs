7be4305bb307d29d467b4b46494bcfac708b9627
Pardis Habibi <130221474+PardisHabibi@users.noreply.github.com>
2025-01-04 15:05:50 +0000

    Fixed floating enemies
    fixed floating enemies in medium and hard. Fixed towers in the floor in medium. Added IEnumerators for each enemy to initialise health using scriptable objects

--- Assets/scripts/Enemy/EnemySpawner.cs
+++ Assets/scripts/Enemy/EnemySpawner.cs
@@ -21,14 +21,14 @@ public class EnemySpawner : MonoBehaviour
         GameObject enemy = Instantiate(EnemyPrefab, position.transform.position, Quaternion.identity);
         Debug.Log("Enemy instantiated: " + enemy.name);
 
-        StartCoroutine(InitializeEnemyAfterDelay(enemy));
+        StartCoroutine(InitializeStandardEnemyAfterDelay(enemy));
     }
 
     //spawn fast enemies
     public void SpawnEnemiesFast()
     {
         GameObject enemy = Instantiate(EnemyPrefabFast, position.transform.position, Quaternion.identity);
-        StartCoroutine(InitializeEnemyAfterDelay(enemy));
+        StartCoroutine(InitializeFastEnemyAfterDelay(enemy));
 
     }
 
@@ -36,10 +36,10 @@ public class EnemySpawner : MonoBehaviour
     public void SpawnEnemiesTank()
     {
         GameObject enemy = Instantiate(EnemyPrefabTank, position.transform.position, Quaternion.identity);
-        StartCoroutine(InitializeEnemyAfterDelay(enemy));
+        StartCoroutine(InitializeTankEnemyAfterDelay(enemy));
     }
 
-    private IEnumerator InitializeEnemyAfterDelay(GameObject enemy)
+    private IEnumerator InitializeStandardEnemyAfterDelay(GameObject enemy)
     {
         // Wait for one frame to ensure that all components are fully initialized
         yield return null;
@@ -49,6 +49,38 @@ public class EnemySpawner : MonoBehaviour
         {
             Debug.Log("Initializing enemy with health: " + enemyStats.standardHealth);
             enemyMovement.Initialize(enemyStats.standardHealth);
+        }
+        else
+        {
+            Debug.LogError("EnemyMovement component not found!");
+        }
+    }
+    private IEnumerator InitializeFastEnemyAfterDelay(GameObject enemy)
+    {
+        // Wait for one frame to ensure that all components are fully initialized
+        yield return null;
+
+        EnemyMovement enemyMovement = enemy.GetComponent<EnemyMovement>();
+        if (enemyMovement != null)
+        {
+            Debug.Log("Initializing enemy with health: " + enemyStats.fastHealth);
+            enemyMovement.Initialize(enemyStats.fastHealth);
+        }
+        else
+        {
+            Debug.LogError("EnemyMovement component not found!");
+        }
+    }
+    private IEnumerator InitializeTankEnemyAfterDelay(GameObject enemy)
+    {
+        // Wait for one frame to ensure that all components are fully initialized
+        yield return null;
+
+        EnemyMovement enemyMovement = enemy.GetComponent<EnemyMovement>();
+        if (enemyMovement != null)
+        {
+            Debug.Log("Initializing enemy with health: " + enemyStats.tankHealth);
+            enemyMovement.Initialize(enemyStats.tankHealth);
         }
         else
         {
